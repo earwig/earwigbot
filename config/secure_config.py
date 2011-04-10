13246a1ef0c0e9728bb11689d6f757d4017271a8
Ben Kurtovic <ben.kurtovic@verizon.net>
2011-04-10 02:12:48 -0400

    adding an empty secure config file so cloners can get the template

---
+++ config/secure_config.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8  -*-
+
+# EarwigBot Configuration File
+# This file contains information that should be kept hidden, including passwords.
+
+# IRC: identify ourselves to NickServ?
+NS_AUTH = False
+NS_USER = ""
+NS_PASS = ""
