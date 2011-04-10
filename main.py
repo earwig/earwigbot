a9a06f2d72f63b4dd1adb6be6c344467ec5e23a2
Ben Kurtovic <ben.kurtovic@verizon.net>
2011-04-10 02:23:19 -0400

    moving secure_config to a default location, also adding a check to see if it's been filled out in main.py

--- main.py
+++ main.py
@@ -2,6 +2,13 @@
 
 from subprocess import *
 
+try:
+    from config.secure_config import *
+except ImportError:
+    print "Can't find a secure_config file!"
+    print "Make sure you have configured the bot by moving 'config/secure_config.py.default' to 'config/secure_config.py' and by filling out the information inside."
+    exit()
+
 while 1:
     cmd = ['python', 'bot.py']
     call(cmd)
