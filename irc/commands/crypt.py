1e89d2a06f4250437298d95a084636cd32c45a0e
Ben Kurtovic <ben.kurtovic@verizon.net>
2011-06-18 01:38:23 -0400

    new blowfish.py module, some changes in Cryptography(), LICENSE cleanup

--- irc/commands/crypt.py
+++ irc/commands/crypt.py
@@ -5,6 +5,7 @@
 import hashlib
 
 from irc.base_command import BaseCommand
+from lib import blowfish
 
 class Cryptography(BaseCommand):
     def get_hooks(self):
@@ -13,11 +14,16 @@ class Cryptography(BaseCommand):
     def get_help(self, command):
         if command == "hash":
             return ("Return the hash of a string using a given algorithm, " +
-                    "e.g. '!hash sha512 Hello world!'.")
+                    "e.g. '!hash sha512 Hello world!'. Use '!hash list' for " +
+                    "a list of supported algorithms.")
+        elif command == "encrypt":
+            return ("Encrypt any string with a given key using an " + 
+                    "implementation of Blowfish, e.g. '!encrypt some_key " + 
+                    "Hello world!'.")
         else:
-            return ("{0} a string with a given key using a given algorithm, " +
-                    "e.g. '!{1} blowfish some_key Hello world!'.").format(
-                    command.capitalize(), command)
+            return ("Decrypt a string with a given key using a given " +
+                    "algorithm, e.g. '!decrypt blowfish some_key Hello " +
+                    "world!'.")
 
     def check(self, data):
         if data.is_command and data.command in ["hash", "encrypt", "decrypt"]:
@@ -26,18 +32,32 @@ class Cryptography(BaseCommand):
 
     def process(self, data):
         if not data.args:
-            self.connection.reply(data, "what do you want me to do?")
+            self.connection.reply(data, "what do you want me to {0}?".format(
+                    data.command))
             return
 
         if data.command == "hash":
             algo = data.args[0]
-            if algo in hashlib.algorithms:
+            if algo == "list":
+                algos = ', '.join(hashlib.algorithms)
+                self.connection.reply(data, "supported algorithms: " + algos +
+                        ".")
+            elif algo in hashlib.algorithms:
                 string = ' '.join(data.args[1:])
                 result = eval("hashlib.{0}(string)".format(algo)).hexdigest()
                 self.connection.reply(data, result)
             else:
-                self.connection.reply(data, "unknown algorithm: {0}".format(
+                self.connection.reply(data, "unknown algorithm: '{0}'.".format(
                         algo))
 
         else:
-            self.connection.reply(data, "not implemented yet!")
+            key = data.args[0]
+            text = ' '.join(data.args[1:])
+            if not text:
+                self.connection.reply(data, "that's a key, yes, but what do " +
+                        " you want me to {0}?".format(data.command))
+                return
+            if data.command == "encrypt":
+                self.connection.reply(data, blowfish.encrypt(key, text))
+            else:
+                self.connection.reply(data, blowfish.decrypt(key, text))
