12c71675d378ea4bf59a0409f87450a5d0d63470
Ben Kurtovic <ben.kurtovic@verizon.net>
2011-07-17 01:04:04 -0400

    adding a !remind command from old EarwigBot, which spawns a new thread that sleeps until its time is up, so impimplementing in !tasks list; {} -> {0}, etc in tasks.py for Python 2.6 support; stripping indentation on blank lines in tasks.py

---
+++ irc/commands/remind.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8  -*-
+
+"""
+Set a message to be repeated to you in a certain amount of time.
+"""
+
+import threading
+import time
+
+from irc.classes import BaseCommand
+
+class Remind(BaseCommand):
+    def get_hooks(self):
+        return ["msg"]
+
+    def get_help(self, command):
+        return "Set a message to be repeated to you in a certain amount of time."
+
+    def check(self, data):
+        if data.is_command and data.command in ["remind", "reminder"]:
+            return True
+        return False
+
+    def process(self, data):
+        if not data.args:
+            self.connection.reply(data, "please specify a time (in seconds) and a message in the following format: !remind <time> <msg>.")
+            return
+
+        try:
+            wait = int(data.args[0])
+        except ValueError:
+            self.connection.reply(data, "the time must be given as an integer, in seconds.")
+            return
+        content = ' '.join(data.args[1:])
+        if not content:
+            self.connection.reply(data, "what message do you want me to give you when time is up?")
+            return
+
+        end_time = time.strftime("%b %d %H:%M:%S", time.localtime(time.time() + wait))
+        end_time_with_timezone = time.strftime("%b %d %H:%M:%S %Z", time.localtime(time.time() + wait))
+        self.connection.reply(data, 'Set reminder for "{0}" in {1} seconds (ends {2}).'.format(message, wait, end_time_with_timezone))
+
+        t_reminder = threading.Thread(target=self.reminder, args=(data, message, wait))
+        t_reminder.name = "reminder " + end_time
+        t_reminder.daemon = True
+        t_reminder.start()
+
+    def reminder(self, data, message, wait):
+        time.sleep(wait)
+        self.connection.reply(data, message)
--- irc/commands/tasks.py
+++ irc/commands/tasks.py
@@ -33,85 +33,87 @@ class Tasks(BaseCommand):
             else:
                 self.connection.reply(data, "no arguments provided. Maybe you wanted '!{cmnd} list', '!{cmnd} start', or '!{cmnd} listall'?".format(cmnd=data.command))
             return
-        
+
         if data.args[0] == "list":
             self.do_list()
-        
+
         elif data.args[0] == "start":
             self.do_start()
-        
+
         elif data.args[0] in ["listall", "all"]:
             self.do_listall()
 
         else: # they asked us to do something we don't know
-            self.connection.reply(data, "unknown argument: \x0303{}\x0301.".format(data.args[0]))
+            self.connection.reply(data, "unknown argument: \x0303{0}\x0301.".format(data.args[0]))
 
     def do_list(self):
         threads = threading.enumerate()
-        
+
         normal_threads = []
         task_threads = []
-        
+
         for thread in threads:
             tname = thread.name
             if tname == "MainThread":
                 tname = self.get_main_thread_name()
-                normal_threads.append("\x0302{}\x0301 (as main thread, id {})".format(tname, thread.ident))
+                normal_threads.append("\x0302{0}\x0301 (as main thread, id {1})".format(tname, thread.ident))
             elif tname in ["irc-frontend", "irc-watcher", "wiki-scheduler"]:
-                normal_threads.append("\x0302{}\x0301 (id {})".format(tname, thread.ident))
+                normal_threads.append("\x0302{0}\x0301 (id {1})".format(tname, thread.ident))
+            elif tname.startswith("reminder"):
+                normal_threads.append("\x0302reminder\x0301 (until {0})".format(tname.replace("reminder ", "")))
             else:
                 tname, start_time = re.findall("^(.*?) \((.*?)\)$", tname)[0]
-                task_threads.append("\x0302{}\x0301 (id {}, since {})".format(tname, thread.ident, start_time))
-        
+                task_threads.append("\x0302{0}\x0301 (id {1}, since {2})".format(tname, thread.ident, start_time))
+
         if task_threads:
-            msg = "\x02{}\x0F threads active: {}, and \x02{}\x0F task threads: {}.".format(len(threads), ', '.join(normal_threads), len(task_threads), ', '.join(task_threads))
+            msg = "\x02{0}\x0F threads active: {1}, and \x02{2}\x0F task threads: {3}.".format(len(threads), ', '.join(normal_threads), len(task_threads), ', '.join(task_threads))
         else:
-            msg = "\x02{}\x0F threads active: {}, and \x020\x0F task threads.".format(len(threads), ', '.join(normal_threads))
+            msg = "\x02{0}\x0F threads active: {1}, and \x020\x0F task threads.".format(len(threads), ', '.join(normal_threads))
         self.connection.reply(self.data, msg)
-    
+
     def do_listall(self):
         tasks = task_manager.task_list.keys()
         threads = threading.enumerate()
         tasklist = []
-        
+
         tasks.sort()
 
         for task in tasks:
             threads_running_task = [t for t in threads if t.name.startswith(task)]
             ids = map(lambda t: str(t.ident), threads_running_task)
             if not ids:
-                tasklist.append("\x0302{}\x0301 (idle)".format(task))
+                tasklist.append("\x0302{0}\x0301 (idle)".format(task))
             elif len(ids) == 1:
-                tasklist.append("\x0302{}\x0301 (\x02active\x0F as id {})".format(task, ids[0]))
+                tasklist.append("\x0302{0}\x0301 (\x02active\x0F as id {1})".format(task, ids[0]))
             else:
-                tasklist.append("\x0302{}\x0301 (\x02active\x0F as ids {})".format(task, ', '.join(ids)))
-        
+                tasklist.append("\x0302{0}\x0301 (\x02active\x0F as ids {1})".format(task, ', '.join(ids)))
+
         tasklist = ", ".join(tasklist)
-        
-        msg = "{} tasks loaded: {}.".format(len(tasks), tasklist)
+
+        msg = "{0} tasks loaded: {1}.".format(len(tasks), tasklist)
         self.connection.reply(self.data, msg)
-    
+
     def do_start(self):
         data = self.data
-        
+
         try:
             task_name = data.args[1]
         except IndexError: # no task name given
             self.connection.reply(data, "what task do you want me to start?")
             return
-        
+
         try:
             data.parse_kwargs()
         except KwargParseException, arg:
-            self.connection.reply(data, "error parsing argument: \x0303{}\x0301.".format(arg))
+            self.connection.reply(data, "error parsing argument: \x0303{0}\x0301.".format(arg))
             return
-        
+
         if task_name not in task_manager.task_list.keys(): # this task does not exist or hasn't been loaded
-            self.connection.reply(data, "task could not be found; either wiki/tasks/{}.py doesn't exist, or it wasn't loaded correctly.".format(task_name))
+            self.connection.reply(data, "task could not be found; either wiki/tasks/{0}.py doesn't exist, or it wasn't loaded correctly.".format(task_name))
             return
-        
+
         task_manager.start_task(task_name, **data.kwargs)
-        self.connection.reply(data, "task \x0302{}\x0301 started.".format(task_name))
+        self.connection.reply(data, "task \x0302{0}\x0301 started.".format(task_name))
 
     def get_main_thread_name(self):
         """Return the "proper" name of the MainThread; e.g. "irc-frontend" or "irc-watcher"."""
