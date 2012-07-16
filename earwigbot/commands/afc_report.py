c3138fd14cf00f4467f2df94994cceb85f65dee8
Justin Yulli Kim <yulli@yulli.org>
2012-07-16 18:45:25 -0400

    Capitalise replies in !report

--- earwigbot/commands/afc_report.py
+++ earwigbot/commands/afc_report.py
@@ -66,7 +66,7 @@ class AFCReport(Command):
         if page:
             return self.report(page)
 
-        self.reply(data, "submission \x0302{0}\x0F not found.".format(title))
+        self.reply(data, "Submission \x0302{0}\x0F not found.".format(title))
 
     def get_page(self, title):
         page = self.site.get_page(title, follow_redirects=False)
