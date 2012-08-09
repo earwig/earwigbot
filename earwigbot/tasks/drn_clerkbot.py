6d3048a6e442df29847783d1d7565d92cefd6689
Ben Kurtovic <ben.kurtovic@verizon.net>
2012-08-09 15:48:03 -0400

    Improve regex for matching signatures.

--- earwigbot/tasks/drn_clerkbot.py
+++ earwigbot/tasks/drn_clerkbot.py
@@ -456,9 +456,9 @@ class DRNClerkBot(Task):
 
         Signatures are returned as tuples of (editor, timestamp as datetime).
         """
-        regex = r"\[\[(?:User(?:\stalk)?\:|Special\:Contributions\/)"
+        regex = r"\[\[(?:[uU]ser(?:\stalk)?\:|[sS]pecial\:[cC]ontributions\/)"
         regex += r"([^\n\[\]|]{,256}?)(?:\||\]\])"
-        regex += r"(?!.*?(?:User(?:\stalk)?\:|Special\:Contributions\/).*?)"
+        regex += r"(?!.*?(?:[uU]ser(?:\stalk)?\:|[sS]pecial\:[cC]ontributions\/).*?)"
         regex += r".{,256}?(\d{2}:\d{2},\s\d{1,2}\s\w+\s\d{4}\s\(UTC\))"
         matches = re.findall(regex, text, re.U)
         signatures = []
