4e8be871b7d57e5512854240baefd1d3bf4268be
Ben Kurtovic <ben.kurtovic@gmail.com>
2015-01-30 12:25:47 -0600

    Update copyright year for 2015.

--- docs/conf.py
+++ docs/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'EarwigBot'
-copyright = u'2009, 2010, 2011, 2012, 2013, 2014 Ben Kurtovic'
+copyright = u'2009-2015 Ben Kurtovic'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
--- earwigbot/__init__.py
+++ earwigbot/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8  -*-
 #
-# Copyright (C) 2009-2014 Ben Kurtovic <ben.kurtovic@gmail.com>
+# Copyright (C) 2009-2015 Ben Kurtovic <ben.kurtovic@gmail.com>
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -30,7 +30,7 @@ details. This documentation is also available `online
 """
 
 __author__ = "Ben Kurtovic"
-__copyright__ = "Copyright (C) 2009, 2010, 2011, 2012, 2013, 2014 Ben Kurtovic"
+__copyright__ = "Copyright (C) 2009-2015 Ben Kurtovic"
 __license__ = "MIT License"
 __version__ = "0.2.dev"
 __email__ = "ben.kurtovic@gmail.com"
